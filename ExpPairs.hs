d286d163b1c9377354fdfdb84a525fa299903e7e
Bodigrim <andrew.lelechenko@gmail.com>
2015-01-03 21:54:38 +0200

    Clean up ExpPairs.hs

--- ExpPairs.hs
+++ ExpPairs.hs
@@ -1,12 +1,3 @@
-import Data.Ratio
-import Data.Ord
-import Data.List
-import Data.Monoid
+module ExpPairs where
 
 import ExpPairs.Optimize
-import ExpPairs.Kratzel
-import ExpPairs.MenzerNowak
-import ExpPairs.Ivic
-
-main :: IO()
-main = print $ searchMinAbscissa [(4,2),(5,4),(6,2),(7,6)]
