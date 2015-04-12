6116ce900e0c69b962c76f0e62c3824e239ab081
Bodigrim <andrew.lelechenko@gmail.com>
2015-04-12 19:49:50 +0300

    Better pattern matching in RatioInf instances

--- Math/ExpPairs/RatioInf.hs
+++ Math/ExpPairs/RatioInf.hs
@@ -33,7 +33,7 @@ instance (Integral t, Show t) => Show (RatioInf t) where
 	show (Finite x) = show x
 	show InfPlus    = "+Inf"
 
-instance (Integral t) => Num (RatioInf t) where
+instance Integral t => Num (RatioInf t) where
 	InfMinus + InfPlus = error "Cannot add up negative and positive infinities"
 	InfPlus + InfMinus = error "Cannot add up negative and positive infinities"
 	InfMinus + _ = InfMinus
@@ -42,7 +42,7 @@ instance (Integral t) => Num (RatioInf t) where
 	_ + InfPlus  = InfPlus
 	(Finite a) + (Finite b) = Finite (a+b)
 
-	fromInteger n = Finite (fromInteger n)
+	fromInteger = Finite . fromInteger
 
 	signum InfMinus   = Finite (-1)
 	signum InfPlus    = Finite 1
@@ -56,48 +56,56 @@ instance (Integral t) => Num (RatioInf t) where
 	negate InfPlus    = InfMinus
 	negate (Finite r) = Finite (negate r)
 
-	InfMinus * a
-		| signum a == Finite 0    = error "Cannot multiply infinity by zero"
-		| signum a == Finite 1    = InfMinus
-		| signum a == Finite (-1) = InfPlus
-	InfPlus  * a
-		| signum a == Finite 0    = error "Cannot multiply infinity by zero"
-		| signum a == Finite 1    = InfPlus
-		| signum a == Finite (-1) = InfMinus
-	a * InfMinus
-		| signum a == Finite 0    = error "Cannot multiply infinity by zero"
-		| signum a == Finite 1    = InfMinus
-		| signum a == Finite (-1) = InfPlus
-	a * InfPlus
-		| signum a == Finite 0    = error "Cannot multiply infinity by zero"
-		| signum a == Finite 1    = InfPlus
-		| signum a == Finite (-1) = InfMinus
-	(Finite a) * (Finite b)     = Finite (a*b)
+	InfMinus * InfMinus = InfMinus
+	InfMinus * InfPlus  = InfMinus
+	InfMinus * Finite a = case signum a of
+		1  -> InfMinus
+		-1 -> InfPlus
+		_  -> error "Cannot multiply infinity by zero"
 
-instance (Integral t) => Fractional (RatioInf t) where
+	InfPlus * InfMinus = InfMinus
+	InfPlus * InfPlus  = InfPlus
+	InfPlus * Finite a = case signum a of
+		1  -> InfPlus
+		-1 -> InfMinus
+		_  -> error "Cannot multiply infinity by zero"
+
+	Finite a * InfMinus = case signum a of
+		1  -> InfMinus
+		-1 -> InfPlus
+		_  -> error "Cannot multiply infinity by zero"
+
+	Finite a * InfPlus = case signum a of
+		1  -> InfPlus
+		-1 -> InfMinus
+		_  -> error "Cannot multiply infinity by zero"
+
+	Finite a * Finite b = Finite (a * b)
+
+instance Integral t => Fractional (RatioInf t) where
 	fromRational = Finite . fromRational
 
 	InfMinus / InfMinus = error "Cannot divide infinity by infinity"
 	InfMinus / InfPlus  = error "Cannot divide infinity by infinity"
-	InfMinus / (Finite a)
-		| signum a ==  0 = error "Cannot divide infinity by zero"
-		| signum a ==  1 = InfMinus
-		| signum a == -1 = InfPlus
+	InfMinus / Finite a = case signum a of
+		1  -> InfMinus
+		-1 -> InfPlus
+		_  -> error "Cannot divide infinity by zero"
 
 	InfPlus  / InfMinus = error "Cannot divide infinity by infinity"
 	InfPlus  / InfPlus  = error "Cannot divide infinity by infinity"
-	InfPlus / (Finite a)
-		| signum a ==  0 = error "Cannot divide infinity by zero"
-		| signum a ==  1 = InfPlus
-		| signum a == -1 = InfMinus
+	InfPlus / Finite a  = case signum a of
+		1  -> InfPlus
+		-1 -> InfMinus
+		_  -> error "Cannot divide infinity by zero"
 
-	(Finite _) / InfPlus  = Finite 0
-	(Finite _) / InfMinus = Finite 0
+	Finite _ / InfPlus  = Finite 0
+	Finite _ / InfMinus = Finite 0
 
-	(Finite _) / (Finite 0) = error "Cannot divide finite value by zero"
-	(Finite a) / (Finite b) = Finite (a/b)
+	Finite _ / Finite 0 = error "Cannot divide finite value by zero"
+	Finite a / Finite b = Finite (a/b)
 
-instance (Integral t) => Real (RatioInf t) where
+instance Integral t => Real (RatioInf t) where
 	toRational (Finite r) = toRational r
 	toRational InfPlus    = error "Cannot map infinity into Rational"
 	toRational InfMinus   = error "Cannot map infinity into Rational"
