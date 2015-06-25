d510e909ff1258b6f5233d763ca98eba175547e1
Bodigrim <andrew.lelechenko@gmail.com>
2015-06-25 21:49:26 +0300

    Rectify PrettyProcess

--- Math/ExpPairs/PrettyProcess.hs
+++ Math/ExpPairs/PrettyProcess.hs
@@ -1,53 +1,85 @@
+{-|
+Module      : Math.ExpPairs.PrettyProcess
+Description : Compact representation of process sequences
+Copyright   : (c) Andrew Lelechenko, 2015
+License     : GPL-3
+Maintainer  : andrew.lelechenko@gmail.com
+Stability   : experimental
+Portability : TemplateHaskell
+
+Transforms sequences of 'Process' into most compact (by the means of typesetting) representation using brackets and powers.
+E. g., AAAABABABA -> A^4(BA)^3.
+
+This module uses memoization extensively.
+-}
+{-# LANGUAGE LambdaCase      #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Math.ExpPairs.PrettyProcess
-	( prettifyM,
+	( prettify,
+		uglify,
 		PrettyProcess) where
 
-import Data.List                (minimumBy, unfoldr)
+import Data.List                (minimumBy)
 import Data.Ord                 (comparing)
 import Data.Function.Memoize    (memoize, deriveMemoizable)
+import Text.PrettyPrint.Leijen
 
 import Math.ExpPairs.ProcessMatrix
 
-data PrettyProcess = Simply [Process] | Repeat PrettyProcess Int | Sequence PrettyProcess PrettyProcess
+-- | Compact representation of the sequence of 'Process'.
+data PrettyProcess
+	= Simply [Process]
+	| Repeat PrettyProcess Int
+	| Sequence PrettyProcess PrettyProcess
+	deriving (Show)
+
+data PrettyProcessWithWidth = PPWL { ppwlProcess :: PrettyProcess, ppwlWidth :: Int }
 
 deriveMemoizable ''PrettyProcess
 
-data PrettyProcessWithLength = PPWL Int PrettyProcess
+instance Pretty PrettyProcess where
+	pretty = \case
+		Simply xs    -> hsep (map (text . show) xs)
+		Repeat _  0  -> empty
+		Repeat xs 1  -> pretty xs
+		Repeat (Simply [A]) n -> text (show A) <> char '^' <> int n
+		Repeat xs n  -> parens (pretty xs) <> char '^' <> int n
+		Sequence a b -> pretty a <+> pretty b
 
-instance Show PrettyProcess where
-	show (Simply xs) = concatMap show xs
-	show (Repeat _  0) = ""
-	show (Repeat xs 1) = show xs
-	show (Repeat xs n) = case show xs of
-		[ch] -> ch : '^' : show n
-		chs  -> '(' : chs ++ (')' : '^' : show n)
-	show (Sequence a b) = show a ++ show b
-
+-- | Width of the bracket.
 bracketWidth :: Int
 bracketWidth = 4
+
+-- | Width of the subscript-sized character (e. g., power).
 subscriptWidth :: Int
 subscriptWidth = 4
-printedLength' :: Process -> Int
-printedLength' A = 10
-printedLength' BA = 20
 
-printedLength :: PrettyProcess -> Int
-printedLength (Simply xs) = sum (map printedLength' xs)
-printedLength (Repeat _ 0) = 0
-printedLength (Repeat xs 1) = printedLengthM xs
-printedLength (Repeat (Simply [A]) n) = printedLength' A + subscriptWidth
-printedLength (Repeat xs n) = printedLength xs + bracketWidth * 2 + subscriptWidth
-printedLength (Sequence a b) = printedLengthM a + printedLengthM b
+-- | Width of the processes in typeset
+processWidth :: Process -> Int
+processWidth  A = 10
+processWidth BA = 20
 
-printedLengthM = memoize printedLength
+-- | Compute the width of the 'PrettyProcess' according to 'bracketWidth', 'subscriptWidth' and 'printedWidth''.
+printedWidth :: PrettyProcess -> Int
+printedWidth = \case
+	Simply xs             -> sum (map processWidth xs)
+	Repeat _ 0            -> 0
+	Repeat xs 1           -> printedWidth xs
+	Repeat (Simply [A]) _ -> processWidth A + subscriptWidth
+	Repeat xs _           -> printedWidth xs + bracketWidth * 2 + subscriptWidth
+	Sequence a b          -> printedWidth a + printedWidth b
 
--- | Returns non-trivial divisors of an argument.
+-- | Convert 'PrettyProcess' to 'PrettyProcessWithWidth'.
+annotateWithWidth :: PrettyProcess -> PrettyProcessWithWidth
+annotateWithWidth p = PPWL p (printedWidth p)
+
+-- | Return non-trivial divisors of an argument.
 divisors :: Int -> [Int]
 divisors n = ds1 ++ reverse ds2 where
 	(ds1, ds2) = unzip [ (a, n `div` a) | a <- [1 .. sqrtint n], n `mod` a == 0 ]
 	sqrtint = round . sqrt . fromIntegral
 
+-- | Try to represent list as a replication of list.
 asRepeat :: [Process] -> ([Process], Int)
 asRepeat [] = ([], 0)
 asRepeat xs = pair where
@@ -55,20 +87,38 @@ asRepeat xs = pair where
 	candidates = [ (take d xs, l `div` d) | d <- divisors l ]
 	pair = head $ filter (\(ys, n) -> concat (replicate n ys) == xs) candidates
 
+-- | Find the most compact representation of the sequence of processes.
 prettify :: [Process] -> PrettyProcess
-prettify []   = Simply []
-prettify [A]  = Simply [A]
-prettify [BA] = Simply [BA]
-prettify xs = minimumBy (comparing printedLengthM) yss where
-	xs'' = case asRepeat xs of
-		(_, 1)   -> Simply xs
-		(xs', n) -> Repeat (prettifyM xs') n
-	yss = xs'' : map f bcs
+prettify = ppwlProcess . prettifyP
 
-	bcs :: [([Process], [Process])]
-	bcs = takeWhile (not . null . snd) $ iterate bcf ([head xs], tail xs)
-	bcf (xs, y:ys) = (xs++[y], ys)
+-- | Find the most compact representation of the sequence of processes, keeping track of widthess.
+prettifyP :: [Process] -> PrettyProcessWithWidth
+prettifyP = memoize prettify' where
 
-	f (bs, cs) = Sequence (prettifyM bs) (prettifyM cs)
+prettify' :: [Process] -> PrettyProcessWithWidth
+prettify' = \case
+	[]   -> annotateWithWidth (Simply [])
+	[A]  -> annotateWithWidth (Simply [A])
+	[BA] -> annotateWithWidth (Simply [BA])
+	xs   -> minimumBy (comparing ppwlWidth) yss where
+		xs'' = case asRepeat xs of
+			(_, 1)   -> annotateWithWidth (Simply xs)
+			(xs', n) -> annotateWithWidth (Repeat (prettify xs') n)
 
-prettifyM = memoize prettify
+		yss = xs'' : map f bcs
+
+		bcs = takeWhile (not . null . snd) $ iterate bcf ([head xs], tail xs)
+
+		bcf (_, [])    = undefined
+		bcf (zs, y:ys) = (zs++[y], ys)
+
+		f (bs, cs) = PPWL (Sequence bsP csP) (bsW + csW) where
+			PPWL bsP bsW = prettifyP bs
+			PPWL csP csW = prettifyP cs
+
+-- | Unfold back 'PrettyProcess' into the sequence of 'Process'.
+uglify :: PrettyProcess -> [Process]
+uglify = \case
+	Simply xs      -> xs
+	Repeat xs n    -> concat . replicate n . uglify $ xs
+	Sequence xs ys -> uglify xs ++ uglify ys
