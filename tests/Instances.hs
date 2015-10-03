e9036d7c2c6a5fae5da63686aa62fc688b680d71
Bodigrim <andrew.lelechenko@gmail.com>
2015-10-03 19:40:02 +0300

    Fix Serial instance for Pair

--- tests/Instances.hs
+++ tests/Instances.hs
@@ -54,20 +54,14 @@ newtype Ratio01 t = Ratio01 t
   deriving (Eq, Ord, Generic)
 
 instance (Ord t, Fractional t, Arbitrary t) => Arbitrary (Ratio01 t) where
-  arbitrary = (Ratio01 . ratio01) <$> arbitrary
+  arbitrary = Ratio01 <$> (arbitrary `suchThat` (\x -> 0 <= x && x <= 1))
   shrink = genericShrink
 
 instance (Ord t, Fractional t, Serial m t) => Serial m (Ratio01 t) where
-  series = cons1 (Ratio01 . ratio01)
+  series = Ratio01 <$> (series `suchThatSerial` (\x -> 0 <= x && x <= 1))
 
 instance Show t => Show (Ratio01 t) where
   showsPrec n (Ratio01 x) = showsPrec n x
-
-ratio01 :: (Fractional a, Ord a) => a -> a
-ratio01 a
-  | abs a <= 1 = recip 2 + a/4
-  | a < 0      = recip (negate a) / 4
-  | otherwise  = 3 * recip 4 + recip a / 4
 
 instance (Ord t, Fractional t, Arbitrary t) => Arbitrary (InitPair' t) where
   arbitrary = f <$> liftM2 (,) arbitrary arbitrary where
@@ -80,8 +74,13 @@ instance (Ord t, Fractional t, Arbitrary t) => Arbitrary (InitPair' t) where
         y' = y*(1-x)
   shrink = genericShrink
 
-instance Serial m t => Serial m (InitPair' t) where
-  series = cons0 Corput01 \/ cons0 Corput12 \/ cons2 Mix
+instance (Ord t, Fractional t, Serial m t) => Serial m (InitPair' t) where
+  series = cons0 Corput01 \/ cons0 Corput12 \/ mseries
+    where
+      mseries = do
+        (Ratio01 x) <- series
+        (Ratio01 y) <- series
+        return $ Mix x (y * (1-x))
 
 instance (Num a, Ord a, Arbitrary a) => Arbitrary (Positive a) where
   arbitrary = Positive <$> (arbitrary `suchThat` (> 0))
